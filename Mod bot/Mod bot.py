c209dec89dd8fc9f53783c3b7cdeaa52a7881b88
Noot Noot <95957330+NootNootpengu@users.noreply.github.com>
2023-05-16 23:28:39 +0330

    Updated files

--- Mod bot/Mod bot.py
+++ Mod bot/Mod bot.py
@@ -20,14 +20,14 @@ async def on_ready():
     print(f"bot ready for action. {bot.user.name}#{bot.user.discriminator}, {bot.user.id}")
     print(f"the bot is in {len(bot.guilds)} guilds")
     for guilds in bot.guilds:
-        print(f"Guild name: {guilds.name}, Guild ID: {guilds.id}, Guild owner name: {guilds.owner.name}#{guilds.owner.discriminator}, Guild owner ID: {guilds.owner.id}")
+        print(
+            f"Guild name: {guilds.name}, Guild ID: {guilds.id}, Guild owner name: {guilds.owner.name}#{guilds.owner.discriminator}, Guild owner ID: {guilds.owner.id}")
 
 
 @bot.command()
 async def create_invite(ctx):
     invite = await ctx.channel.create_invite(max_age=300)
-    link = discord.utils.resolve_invite(invite)
-    server = await bot.fetch_invite(f"{link}")
+    server = await bot.fetch_invite(f"{invite}")
     await ctx.send(server)
 
 
@@ -35,55 +35,40 @@ async def create_invite(ctx):
 async def members(ctx):
     members = []
     for n in bot.get_all_members():
-        if 1081610766928597093 in n.roles:
+        if n.bot:
             continue
         else:
             members.append(f"{n.name}#{n.discriminator}")
-    members = list(dict.fromkeys(members))
-    members.sort()
-    await ctx.send(members)
+    await ctx.send(f"{len(members)}")
 
 
 @bot.command(description="Ban a user. needed argument is Member and Reason")
+@commands.has_permissions(ban_members=True)
 async def ban(ctx, *, member: discord.Member, reason=f"{None}"):
     global embed
-    if not ctx.author.guild_permissions.ban_members:
+    try:
+        await member.ban(reason=reason)
         embed = discord.Embed(
-            title="Missing Perms",
-            description=f"{ctx.author.mention}, you don't have enough perms."
+            title="Ban",
+            description=f"{member.mention} has been banned by {ctx.author.mention} for {reason}."
         )
+        imageurl = f'{member.display_avatar}'
+        embed.set_image(url=imageurl)
         await ctx.send(embed=embed)
-        return
-    else:
-        try:
-            await member.ban()
-            embed = discord.Embed(
-                title="Ban",
-                description=f"{member.mention} has been banned by {ctx.author.mention} for {reason}."
-            )
-            imageurl = f'{member.display_avatar}'
-            embed.set_image(url=imageurl)
-            await ctx.send(embed=embed)
-        except:
-            await ctx.send("Unsuccessful command")
-        try:
-            await ctx.member.send(embed)
-        except:
-            pass
+    except:
+        await ctx.send("Unsuccessful command")
+    try:
+        await ctx.member.send(embed)
+    except:
+        pass
 
 
 @bot.command(description="Kicks a user. needed argument is Member and Reason")
-async def kick(ctx, *, member: discord.Member, reason=f"[No reason Provided]"):
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, reason=f"[No reason Provided]"):
     global embed
-    if not ctx.author.guild_permissions.kick_members:
-        embed = discord.Embed(
-            title="Missing Perms",
-            description=f"{ctx.author.mention}, you don't have enough perms."
-        )
-        await ctx.send(embed=embed)
-        return
     try:
-        await member.ban()
+        await member.kick(reason=reason)
         embed = discord.Embed(
             title="Kick",
             description=f"{member.mention} has been kicked by {ctx.author.mention} for {reason}."
@@ -100,16 +85,10 @@ async def kick(ctx, *, member: discord.Member, reason=f"[No reason Provided]"):
 
 
 @bot.command(description="Purges a channel")
-async def purge(ctx):
-    if not ctx.author.guild_permissions.manage_channels:
-        embed = discord.Embed(
-            title="Missing Perms",
-            description=f"{ctx.author.mention}, you don't have enough perms."
-        )
-        await ctx.send(embed=embed)
-        return
+@commands.has_permissions(manage_channels=True)
+async def purge(ctx, reason=f"{None}"):
     await ctx.channel.delete()
-    new_channel = await ctx.channel.clone(reason="Channel was purged")
+    new_channel = await ctx.channel.clone(reason=reason)
     await new_channel.edit(position=ctx.channel.position)
     embed = discord.Embed(
         title="Purge",
@@ -118,19 +97,15 @@ async def purge(ctx):
     imageurl = f'{ctx.author.display_avatar}'
     embed.set_image(url=imageurl)
     bot.get_channel(new_channel.id)
-    await ctx.new_channel.send(embed=embed)
+    await new_channel.send(embed=embed)
 
 
 @bot.command(description="times out a user. needed argument are member, seconds, minutes, hours, days and reason")
+@commands.has_permissions(mute_members=True)
 async def mute(ctx, member: discord.Member, seconds: int = 0, minutes: int = 0, hours: int = 0, days: int = 0,
-               reason: str = "Just cause."):
+               reason: str = f"{None}"):
     global embed
     duration = datetime.timedelta(seconds=seconds, minutes=minutes, hours=hours, days=days)
-    if not ctx.author.guild_permissions.timeout_members:
-        embed = discord.Embed(
-            title="Missing Perms",
-            description=f"{ctx.author.mention}, you don't have enough perms."
-        )
     try:
         await member.timeout(duration, reason=reason)
         embed = discord.Embed(
